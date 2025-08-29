03242aed513a94db3dad305098c0164ce09e3265
Michael Sumner <mdsumner@gmail.com>
2025-08-29 03:40:41 +0000

    barebones statewide

--- data-raw/transport-segments.R
+++ data-raw/transport-segments.R
@@ -1,16 +1,22 @@
-munips <- c("CLARENCE", "GLENORCHY", "HOBART", "KINGBOROUGH")
-
-dsn_all <- sprintf("/vsizip//vsicurl/https://listdata.thelist.tas.gov.au/opendata/data/LIST_TRANSPORT_SEGMENTS_%s.zip",
-        munips)
+#munips <- c("CLARENCE", "GLENORCHY", "HOBART", "KINGBOROUGH")
+munips <- c("HUON_VALLEY", "KING_ISLAND", "SOUTHERN_MIDLANDS", "LAUNCESTON",
+  "GEORGE_TOWN", "MEANDER_VALLEY", "TASMAN", "KENTISH", "KINGBOROUGH",
+  "CENTRAL_HIGHLANDS", "DORSET", "WARATAH_WYNYARD", "DEVONPORT",
+  "GLAMORGAN_SPRING_BAY", "CLARENCE", "BREAK_O_DAY", "CIRCULAR_HEAD",
+  "HOBART", "GLENORCHY", "NORTHERN_MIDLANDS", "DERWENT_VALLEY",
+  "BURNIE", "SORELL", "CENTRAL_COAST", "WEST_COAST", "WEST_TAMAR",
+  "FLINDERS", "LATROBE", "BRIGHTON")
+dsn_all <- sprintf("/vsizip/{/vsicurl/https://listdata.thelist.tas.gov.au/opendata/data/LIST_TRANSPORT_SEGMENTS_%s.zip}/list_transport_segments_%s.gdb",
+        munips, tolower(munips))
 
 library(gdalraster)
 #gdal_usage("vector concat")
 delete_after <- c()
 for (i in seq_along(munips)) {
-  args <- list(input = dsn_all, output = outf <- sprintf("%s.parquet", munips[i]), format = "Parquet",
+  args <- list(input = dsn_all[i], output = outf <- sprintf("%s.parquet", munips[i]), format = "Parquet",
                layer = sprintf("list_transport_segments_%s", tolower(munips[i])),
-               `output-layer` = "list_transport_segments", mode = "single")
-  alg <- new(gdalraster::GDALAlg, "vector concat", args)
+               `output-layer` = "list_transport_segments")
+  alg <- new(gdalraster::GDALAlg, "vector convert", args)
 
   alg$run()
 
@@ -21,10 +27,14 @@ for (i in seq_along(munips)) {
 
 ## now merge em
 args <- list(input = delete_after,
-             output = outf <- "LIST_TRANSPORT_SEGMENTS.parquet", format = "Parquet", mode = "single")
+             output = outf <- "LIST_TRANSPORT_SEGMENTS.parquet", format = "Parquet", mode = "single",
+             `creation-option` = c("COMPRESSION=ZSTD"))
 alg <- new(gdalraster::GDALAlg, "vector concat", args)
 
 alg$run()
 
 alg$release()
 unlink(delete_after)
+
+## now upload to github
+#piggyback::pb_upload("LIST_TRANSPORT_SEGMENTS.parquet")
